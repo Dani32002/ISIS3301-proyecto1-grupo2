72cd43a477334ad60a6860a816310b5e2b019fcd
Daniel Escalante <d.escalante@uniandes.edu.co>
2024-04-11 16:35:22 -0500

    Basic structure and routing

--- etapa2/review-classifier/src/App.js
+++ etapa2/review-classifier/src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Header from ',/components/Header'
+import Predict from './components/Predict';
+import Stats from './components/Stats';
+import Landing from './components/Landing';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={ <Landing /> }/>
+          <Route path='/predict' element={ <Predict /> }/>
+          <Route path='/stats' element={ <Stats /> }/>
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
