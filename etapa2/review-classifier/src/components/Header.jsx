d253c95f4e22e861cd4a6aa0a0da0205e98db31f
Daniel Felipe Vargas <d.vargasu@uniandes.edu.co>
2024-04-12 14:50:43 -0500

    improved app appearance

--- etapa2/review-classifier/src/components/Header.jsx
+++ etapa2/review-classifier/src/components/Header.jsx
@@ -1,19 +1,51 @@
 import React from 'react'
 import { Navbar, Container, Nav } from 'react-bootstrap'
+import styled from 'styled-components'
+
+const StyledNavbar = styled(Navbar)`
+  background-color: #333;
+  border: none;
+  padding: 0;
+  height: 70px;
+`;
+
+const StyledNav = styled(Nav)`
+  a {
+    color: #fff;
+    margin-right: 15px;
+
+    &:hover {
+      color: #ddd;
+    }
+  }
+`;
+
+const StyledBrand = styled(Navbar.Brand)`
+  font-weight: bold;
+  display: flex;
+  align-items: center;
+
+  img {
+    margin-right: 10px;
+    width: 30px; // Adjust this value to change the size of the image
+  }
+`;
 
 const Header = () => {
   return (
-    <Navbar bg="light" data-bs-theme="light" className='header'>
-        <Container>
-          <Navbar.Brand href="/">BI ;{"("}</Navbar.Brand>
-          <Nav className="me-auto">
-            <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="/predict">Predict</Nav.Link>
-            <Nav.Link href="/stats">Stats</Nav.Link>
-            <Nav.Link href="/retrain">Retrain</Nav.Link>
-          </Nav>
-        </Container>
-    </Navbar>
+    <StyledNavbar variant="dark">
+      <Container>
+        <StyledBrand href="/">
+          <img src="https://www.pngkey.com/png/full/168-1684318_golf-icon-white-yireh-online-business-solutions-golf.png" alt="Logo" />
+          Hotel Intelligence
+        </StyledBrand>
+        <StyledNav className="me-auto">
+          <Nav.Link href="/predict">Predict</Nav.Link>
+          <Nav.Link href="/stats">Stats</Nav.Link>
+          <Nav.Link href="/retrain">Retrain</Nav.Link>
+        </StyledNav>
+      </Container>
+    </StyledNavbar>
   )
 }
 
--- etapa2/review-classifier/src/components/Landing.jsx
+++ etapa2/review-classifier/src/components/Landing.jsx
@@ -1,8 +1,43 @@
 import React from 'react'
+import styled from 'styled-components'
+
+
+const LandingContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: calc(100vh - 70px); // assuming the navbar height is 60px
+  background-color: #f4f4f4;
+  padding: 20px;
+  box-sizing: border-box;
+  overflow: hidden;
+`;
+
+const Content = styled.div`
+  text-align: center;
+  max-width: 800px;
+`;
+
+const Title = styled.h1`
+  font-size: 2.5em;
+  color: #333;
+`;
+
+const Subtitle = styled.h2`
+  font-size: 1.5em;
+  color: #666;
+`;
 
 const Landing = () => {
   return (
-    <div>Landing</div>
+    <LandingContainer>
+      <Content>
+        <Title>Welcome to our Hotel Management App</Title>
+        <Subtitle>
+          This application will help you improve your management and decision-making by identifying the most important complaints in reviews and points of improvement in your hotel.
+        </Subtitle>
+      </Content>
+    </LandingContainer>
   )
 }
 
