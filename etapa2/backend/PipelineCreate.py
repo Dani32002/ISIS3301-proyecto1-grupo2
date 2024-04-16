854d021915b0eaa7febeebf2362926bea6a9cd49
Daniel Escalante <d.escalante@uniandes.edu.co>
2024-04-15 19:47:11 -0500

    Backend

---
+++ etapa2/backend/PipelineCreate.py
@@ -0,0 +1,151 @@
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.metrics import classification_report
+import pandas as pd
+import re, unicodedata
+import inflect
+import contractions
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.base import BaseEstimator, ClassifierMixin, TransformerMixin
+from sklearn.model_selection import GridSearchCV
+from sklearn.linear_model import LogisticRegression
+import nltk
+import joblib
+from nltk import word_tokenize
+nltk.download('stopwords')
+from nltk.corpus import stopwords
+import spacy
+from sklearn.pipeline import Pipeline
+from sklearn.model_selection import train_test_split
+
+class TextPreprocessor(BaseEstimator, TransformerMixin):
+    def __init__(self):
+        self.stop_words_spanish = set(stopwords.words('spanish'))
+        self.lemmatizer = spacy.load("es_core_news_sm")
+
+    def remove_non_ascii(self, words):
+        return [unicodedata.normalize('NFKD', word).encode('ascii', 'ignore').decode('utf-8', 'ignore') if word is not None else None for word in words]
+
+    def to_lowercase(self, words):
+        return [word.lower() for word in words]
+
+    def remove_punctuation(self, words):
+        return [re.sub(r'[^\w\s]', '', word) if word is not None else None for word in words]
+
+    def replace_numbers(self, words):
+        p = inflect.engine()
+        return [p.number_to_words(word) if word.isdigit() else word for word in words if not any(char.isdigit() for char in word)]
+
+    def remove_stopwords(self, words):
+        return [word for word in words if word not in self.stop_words_spanish]
+
+    def remove_accidentals(self, words):
+        replacements = {'á': 'a', 'é': 'e', 'í': 'i', 'ó': 'o', 'ú': 'u', 'ü': 'u'}
+        return [''.join(replacements[char] if char in replacements else char for char in word) for word in words]
+
+    def lemmatize_verbs(self, words):
+        doc = self.lemmatizer(" ".join(words))
+        return [token.lemma_ for token in doc]
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X):
+        X['Review'] = X['Review'].apply(contractions.fix)
+        X['words'] = X['Review'].apply(word_tokenize)
+        X['words'].dropna(inplace=True)
+        X['words'] = X['words'].apply(self.to_lowercase)
+        X['words'] = X['words'].apply(self.replace_numbers)
+        X['words'] = X['words'].apply(self.remove_punctuation)
+        X['words'] = X['words'].apply(self.remove_non_ascii)
+        X['words'] = X['words'].apply(self.remove_stopwords)
+        X['words'] = X['words'].apply(self.lemmatize_verbs)
+        X['words'] = X['words'].apply(self.remove_accidentals)
+        X['words'] = X['words'].apply(lambda x: ' '.join(map(str, x)))
+        X['words'] = X['words'].apply(lambda x: re.sub(r'\s+', ' ', x))
+        X['words'] = X['words'].apply(lambda x: x.strip())
+        return X
+    
+
+class Vectorizer(BaseEstimator, TransformerMixin):
+    def __init__(self):
+        pass
+
+    def fit(self, X, y=None):
+        tf_idf = TfidfVectorizer()
+        X_tf_idf = tf_idf.fit_transform(X['words'])
+        data_prep = X_tf_idf.copy().todense()
+        data_prep = pd.DataFrame(data_prep, columns=tf_idf.vocabulary_.keys())
+        self.columns=data_prep.columns
+        return self
+
+    def transform(self, X, y=None):
+        tf_idf = TfidfVectorizer()
+        X_tf_idf = tf_idf.fit_transform(X['words'])
+        data_new = pd.DataFrame(X_tf_idf.toarray(), columns=tf_idf.get_feature_names_out())
+        data_new = data_new.reindex(columns=self.columns)
+        data_new.fillna(0, inplace=True)
+
+        # Get the features present in the training data but not in the test data
+        missing_features = set(self.columns) - set(data_new.columns)
+
+        # Get the features present in the test data but not in the training data
+        extra_features = set(data_new.columns) - set(self.columns)
+        for feature in missing_features:
+          data_new[feature] = 0
+        # Drop the extra features from the test data
+        data_new.drop(columns=extra_features, inplace=True)
+        data_new = data_new.apply(lambda x: (x - x.min()) / (x.max() - x.min()), axis=1)
+        return data_new
+
+# Custom estimator for GridSearchCV
+class GridSearchCVWrapper(BaseEstimator, ClassifierMixin):
+    def __init__(self, base_model, param_grid, cv=5):
+        self.base_model = base_model
+        self.param_grid = param_grid
+        self.cv = cv
+        self.grid_search = GridSearchCV(self.base_model, self.param_grid, cv=self.cv)
+
+    def fit(self, X, y=None):
+        self.grid_search.fit(X, y)
+        self.best_estimator_ = self.grid_search.best_estimator_
+        return self
+
+    def predict(self, X):
+        return self.best_estimator_.predict(X)
+    
+    def predict_proba(self, X):
+        return self.best_estimator_.predict_proba(X)
+    
+    def best_estimator_(self):
+        return self.best_estimator_
+  
+param_grid = {
+    'penalty': ['l2'],
+    'C': [3],
+    'solver': ['sag'],
+    'max_iter': [1000]
+}
+
+data=pd.read_csv('tipo1_entrenamiento_estudiantes.csv', sep=',', encoding = 'utf-8')
+label = 'Class'
+features = data.columns.to_list()
+features.remove(label)
+
+x_train, x_test, y_train, y_test = train_test_split(data[features], data[label], test_size=0.2, random_state=1)
+#json_array = data.head(2000).to_json(orient='records')
+#with open("datos.txt", "w") as text_file:
+#    text_file.write(json_array)
+
+"""
+# Define the pipeline with preprocessing and logistic regression model
+pipeline = Pipeline([
+    ('preprocessor', TextPreprocessor()),
+    ('vectorizer', Vectorizer()),
+    ('LogisticRegression', GridSearchCVWrapper(LogisticRegression(), param_grid, cv=2))
+])
+
+# Fit the pipeline and predict
+pipa = pipeline.fit(x_train, y_train)
+y_pred = pipa.predict(x_test)
+print(classification_report(y_test, y_pred))
+joblib.dump(pipeline, 'pipelineReviews.joblib')"""
---
+++ etapa2/backend/main.py
@@ -0,0 +1,170 @@
+from DataModel import DataModel
+import pandas as pd
+from fastapi import FastAPI
+import joblib
+from PipelineCreate import x_test, y_test
+from sklearn.metrics import classification_report
+from typing import List
+from DataModelLabeled import DataModelLabeled
+
+
+app = FastAPI()
+
+model = joblib.load("./assets/pipelineReviews.joblib")
+
+y_pred = model.predict(x_test)
+report = classification_report(y_test, y_pred, output_dict=True)
+
+# Para revisar el estado del api
+# No recibe nada y siempre retorna:
+# {"status": "API is running"}
+@app.get("/")
+def read_root():
+   return {"status": "API is running"}
+
+
+# Recibe una lista de objetos json de la forma:
+"""
+{
+  "Review":"PESIMO LO ODIE",
+  "Class": 1
+}
+"""
+# Re entrena el modelo con estos datos y recalcula las metricas
+@app.post("/retrain")
+def retrain_pipe(data: List[DataModelLabeled]):
+   # Creación del dataframe con las columnas correspondientes
+   df = pd.DataFrame(columns=['Review', 'Class'])
+   
+   #Agregar cada fila al dataframe
+   for element in data:
+      entrada = pd.DataFrame({'Review': [element.Review], 'Class': [element.Class]})
+      df = pd.concat([df, entrada], ignore_index = True)
+   df['Class'] = df['Class'].astype(int)
+   print(df['Class'].dtypes)
+   #Entrenamiento del modelo
+   model.fit(df[['Review']], df['Class'])
+   #Actualización de metricas
+   global y_pred
+   y_pred = model.predict(x_test)
+   global report
+   report = classification_report(y_test, y_pred, output_dict=True)
+   #Guardado del modelo
+   joblib.dump(model, "./assets/pipelineReviews.joblib")
+   return {"status": "exito"}
+
+# Recibe una review en formato json de la siguiente forma 
+# { "Review": "Aca va su reseña" }
+# Retorna la calificación predecida y las probabilidades de cada calificación dadas por el modelo 
+# {"prediction": respuesta, "probabilities": respuesta_proba}
+@app.get("/predict")
+def make_predictions(dataElements: List[DataModel]):
+   valor_retorno = []
+   for dataModel in dataElements:
+      respuesta = 0
+      respuesta_proba = [0,0,0,0,0]
+      try:
+         # Transforma los datos en un dataframe
+         values = dict(dataModel)
+         print(values)
+         data = pd.DataFrame(values['Review'], columns=['Review'], index = [1]) 
+         # Saca las probabilidades
+         respuesta = model.predict(data).tolist()[0]
+         respuesta_proba = model.predict_proba(data).tolist()[0]
+      except ValueError:
+         pass
+      valor_retorno.append({"prediction": respuesta, "probabilities": respuesta_proba})
+   return valor_retorno
+
+def respuesta_metricas(df):
+   # Creación del objeto de retorno con los valores del reporte entregado como un
+   # dataframe
+   resultado = {}
+   elementos = ['general', 'score1', 'score2', 'score3', 'score4', 'score5']
+   subElementos = ['precision', 'recall', 'f1_score']
+   for i in range(0, len(elementos)):
+      fila = 'accuracy' if (elementos[i] == 'general') else elementos[i][5]
+      resultado[elementos[i]] = {}
+      for j in range(0, len(subElementos)):
+         resultado[elementos[i]][subElementos[j]] = df.loc[fila, subElementos[j]]
+   return resultado
+
+def modificacion_palabras(resultado):
+   #Encontrar las palabras más relevantes de cada calificación
+   coeficientes = model['LogisticRegression'].grid_search.best_estimator_.coef_
+   for i, coefs in enumerate(coeficientes):
+      top_indices = coefs.argsort()[-20:]
+      top_features = model['LogisticRegression'].grid_search.best_estimator_.feature_names_in_[top_indices]
+      resultado['score' + str(i+1)]['top_words'] = top_features.tolist()
+   return resultado
+
+# Sirve para ver las metricas actuales del modelo basandose en un set de pruebas
+# con el cual no fue entrenado, para cada calificación retorna precision, recall
+# f1_score y las palabras más relevantes.
+# Ejemplo de respuesta:
+"""{
+    "general": {
+        "precision": 0.4641269841269841,
+        "recall": 0.4641269841269841,
+        "f1_score": 0.4641269841269841
+    },
+    "score1": {
+        "precision": 0.5083333333333333,
+        "recall": 0.40939597315436244,
+        "f1_score": 0.45353159851301117,
+        "top_words": [
+            "demaciadoma",
+            ...
+        ]
+    },
+    "score2": {
+        "precision": 0.3829787234042553,
+        "recall": 0.3287671232876712,
+        "f1_score": 0.3538083538083538,
+        "top_words": [
+            "decepcionado",
+            ...
+        ]
+    },
+    "score3": {
+        "precision": 0.3754152823920266,
+        "recall": 0.35202492211838005,
+        "f1_score": 0.3633440514469453,
+        "top_words": [
+            "esperabar",
+            ...
+        ]
+    },
+    "score4": {
+        "precision": 0.37901498929336186,
+        "recall": 0.45038167938931295,
+        "f1_score": 0.4116279069767442,
+        "top_words": [
+            "calor",
+            ...
+        ]
+    },
+    "score5": {
+        "precision": 0.6172344689378757,
+        "recall": 0.6247464503042597,
+        "f1_score": 0.6209677419354839,
+        "top_words": [
+            "diabet",
+            ...
+        ]
+    }
+}"""
+@app.get("/metrics")
+def show_metrics():
+   #Obtener el reporte y pasarlo a dataframe
+   df = pd.DataFrame(report).transpose()
+   df = df.rename(columns={'f1-score': 'f1_score'})
+
+   # Creación del objeto de retorno con las metricas
+   resultado = respuesta_metricas(df)
+
+   #Inclusión de las palabras más relevantes de cada calificación
+   resultado = modificacion_palabras(resultado)
+   
+   
+   return resultado
